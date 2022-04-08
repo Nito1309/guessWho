750ff770bf84182eca65d62007b60a6938a12b46
Juan Pablo Rodriguez Gutierrez <juanpargz@Juans-MacBook-Pro.local>
2022-04-07 22:00:18 -0500

    Basic html structure of homePage added

--- guess-who/src/App.js
+++ guess-who/src/App.js
@@ -1,12 +1,19 @@
 import{ Stack, Button }from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
+//        <Button variant="contained" startIcon={<AccountCircleIcon/>} >First Button!</Button>
+
 function App() {
   return (
     <div>
-      <h1>First commit</h1>
-      <Stack spacing={2} direction="row">
-        <Button variant="contained" startIcon={<AccountCircleIcon/>} >First Button!</Button>
+      <h1>Guess Who</h1>
+      <AccountCircleIcon/>
+      Nito1309
+      <Stack spacing={2} >
+        <Button variant="contained" >Crear partida</Button>
+        <Button variant="contained" >Unirse a partida</Button>
+        <Button variant="contained" >Iniciar sesi&oacute;n</Button>
+        <Button variant="contained" >Configuraci&oacute;n</Button>
       </Stack>
     </div>
    
