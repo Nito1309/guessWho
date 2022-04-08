33715b049166774190d9784a50d8f82150e1a543
Juan Pablo Rodriguez Gutierrez <juanpargz@Juans-MacBook-Pro.local>
2022-04-07 21:32:22 -0500

    Mui icons-material added

--- guess-who/src/App.js
+++ guess-who/src/App.js
@@ -1,11 +1,12 @@
 import{ Stack, Button }from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function App() {
   return (
     <div>
       <h1>First commit</h1>
       <Stack spacing={2} direction="row">
-        <Button variant="contained">First Button!</Button>
+        <Button variant="contained" startIcon={<AccountCircleIcon/>} >First Button!</Button>
       </Stack>
     </div>
    
