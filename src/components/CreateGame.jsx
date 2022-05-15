7f97fb8088d8551e4eaaa4f8ca2c3e09f4d2612e
Juan Pablo Rodriguez Gutierrez <juanpargz@Juans-MacBook-Pro.local>
2022-05-15 14:22:01 -0500

    back button

--- src/components/CreateGame.jsx
+++ src/components/CreateGame.jsx
@@ -4,7 +4,7 @@ import BackButton from "./form-components/BackButton";
 function CreateGame(){
     return( 
         <div id="parent" className='w-full h-screen bg-background-create-game bg-cover'>
-            <BackButton text={'Prueba'} navigate='/'></BackButton>
+            <BackButton text={'Guess who ?'} bgcolor='#C4C4C4' navigate='/'></BackButton>
         </div>
     )
 }
--- src/components/form-components/BackButton.jsx
+++ src/components/form-components/BackButton.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Button } from "@mui/material";
 import { Link } from 'react-router-dom';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { fontSize } from "@mui/system";
 
-const BackButton = ({text,navigate='' }) => {
+const BackButton = ({text,navigate='' , bgcolor}) => {
     // const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
     //     color: theme.palette.getContrastText(purple[500]),
     //     backgroundColor: purple[500],
@@ -14,7 +16,12 @@ const BackButton = ({text,navigate='' }) => {
     if (navigate!=''){
     return( 
         <Link to={navigate}>
-        <Button className='w-60'  variant="contained" >{`<  ${text}`}</Button>
+        <Button className='w-30 h-18' size="large"  variant="contained" startIcon={<ArrowBackIosIcon/>} 
+        sx={{ bgcolor: bgcolor , fontSize: '20px', 
+        '&:hover': {
+            backgroundColor: 'white',
+          },}}>{text}</Button>
+
         </Link>
     )
     } else {
